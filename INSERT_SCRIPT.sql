0da0655a2ea0620ee0eac0a28ab32aa021a01ce1
Crystal Combs <Crystal Combs>
2017-04-04 17:12:08 -0400

    INSERT_SCRIPT.sql

---
+++ INSERT_SCRIPT.sql
@@ -0,0 +1,30 @@
+/****************************************************************************/
+/*               GROUP PROJECT DATA INSERT SCRIPT                            /
+/*                                                                           /
+/*               Created By: Crystal Combs, Matthew Luc, Charles             /
+/*                           Wigle & Jonathan Quilliams                      /
+/*                                                                           /
+/*               Creation Date: 04-03-2017                                   /
+/*               Last Modified: 04-04-2017                                   /
+/****************************************************************************/
+
+/* RULES for keeping this the same on all of our schemas. Use your assigned
+    id numbers for creating any entries into the database. Make sure all 
+    entries can be tracked to each other and work from the non-foreign key 
+    tables to the outmost tables in the design. 
+    ID NUMBERS:
+        Crystal Combs: 1-25
+        Charles Wigle: 26-50
+        Jonathan Quilliams: 51-75
+        Matthew Luc: 76-100
+        
+        We are using GitHub to keep this as one document. Please commit changes
+        at the end of every session you do and remember to clone it again
+        before you start working on it, as others may have committed 
+        changes!
+        
+        GITHUB LINK: https://github.com/CombsC/3020_GroupProject
+        
+      WE NEED TO REDESIGN THE DATABASE FAIRLY QUICKLY BASED ON THE GRADE 
+      SHEET WE RECIVED FROM DELIVERABLE 1!!!
+  */    
