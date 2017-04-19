85bd656bead7a9329afd051dbbff718554cf7fd7
charlesetsu <zcbw22@etsu.edu>
2017-04-18 22:56:24 -0400

    updated, but with monthly_reports giving an error.

--- ReportCompilation.sql
+++ ReportCompilation.sql
@@ -1,19 +1,69 @@
-set serveroutput on;
+SET serveroutput ON;
 
 CREATE OR REPLACE PACKAGE REPORTS IS
 
+    PROCEDURE ANNUAL_REPORT;
+    PROCEDURE MONTHLY_REPORT(yr VARCHAR2, mnth VARCHAR2); 
     PROCEDURE EVENT_REPORT;
     PROCEDURE CLASS_REP_CONTACT_LIST;
-    PROCEDURE MONTHLY_REPORT; 
     PROCEDURE PHONOTHON_CONTACT_LIST;
-    PROCEDURE EMPLOYER_MATCHES; 
+    PROCEDURE EMPLOYER_MATCHES;
+    PROCEDURE DONORS_CIRCLES_LIST;
+    PROCEDURE SUMMARY_ANNUAL_TOTALS;
 
 END REPORTS;
 /
 
-CREATE OR REPLACE PACKAGE BODY REPORTS IS
+CREATE OR REPLACE PACKAGE BODY REPORTS AS
 
-    PROCEDURE EVENT_REPORT IS
+  PROCEDURE MONTHLY_REPORT(yr VARCHAR2, mnth VARCHAR2) IS
+  
+      CURSOR mReports IS
+  
+      SELECT d.LastName AS LName, d.FirstName AS FName, a.City AS Cty, 
+          a.State AS Ste, a.Zip AS Zp, a.StreetAddress AS SAddress, 
+          a.AptNumber AS AptNum, TO_CHAR(do.Amount_Pledged - do.Amount_Paid, 
+          '9999999.99') AS Amt_Due, TO_CHAR(pp.DueDate, 'DD-MON-YYYY') AS DDate, 
+          TO_CHAR(do.Amount_Pledged, '9999999.99') AS Amt_Pledged, 
+          TO_CHAR(do.Amount_Paid, '9999999.99') AS Amt_Paid, 
+          TO_CHAR(pp.Last_Payment_Date, 'DD-MON-YYYY') AS LPayDate
+      FROM Donor d 
+      INNER JOIN Address a ON d.AddressID = a.AddressID
+      INNER JOIN Donation do ON d.DonorID = do.DonorID
+      INNER JOIN PaymentPlan pp ON do.DonationID = pp.DonationID
+      WHERE TO_CHAR(pp.Last_Payment_Date, 'YYYY') = yr AND
+      TO_CHAR(pp.Last_Payment_Date, 'MON') = mnth AND 
+      TO_CHAR(do.Amount_Pledged - do.Amount_Paid, '9999999.99') > 0
+      ORDER BY Last_Payment_Date;
+      
+      cRow mReports%ROWTYPE;	-- Cursor Row variable
+    
+    BEGIN
+    
+      FOR cRow IN mReports LOOP
+    
+          DBMS_OUTPUT.PUT_LINE('Name : ' || cRow.LName || ', ' || cRow.FName);
+          DBMS_OUTPUT.PUT_LINE('City : ' || cRow.Cty);
+          DBMS_OUTPUT.PUT_LINE('State: ' || cRow.Ste);
+          DBMS_OUTPUT.PUT_LINE('ZIP: ' || cRow.Zp);
+          DBMS_OUTPUT.PUT_LINE('Street Address : ' || cRow.SAddress);
+          DBMS_OUTPUT.PUT_LINE('Apartment Number : ' || cRow.AptNum);
+          DBMS_OUTPUT.PUT_LINE('Amount Due : ' || cRow.Amt_Due);
+          DBMS_OUTPUT.PUT_LINE('Date Due : ' || cRow.DDate);
+          DBMS_OUTPUT.PUT_LINE('Amount Pledged : ' || cRow.Amt_Pledged);
+          DBMS_OUTPUT.PUT_LINE('Amount Received : ' || cRow.Amt_Paid);
+          DBMS_OUTPUT.PUT_LINE('Date of Previous Payment : ' || cRow.LPayDate);
+          FOR j in 1..cRow.LPayDate LOOP
+          DBMS_OUTPUT.PUT_LINE('*');
+          END LOOP;
+          DBMS_OUTPUT.PUT_LINE('');
+          DBMS_OUTPUT.NEW_LINE;
+    
+      END LOOP;
+    
+    END MONTHLY_REPORT;
+    
+  PROCEDURE EVENT_REPORT IS
 
 		CURSOR evReport IS
 
@@ -36,25 +86,21 @@ CREATE OR REPLACE PACKAGE BODY REPORTS IS
 			DBMS_OUTPUT.PUT_LINE('Name : ' || cRow.LName || ', ' || cRow.FName);
 			DBMS_OUTPUT.PUT_LINE('Amount Pledged : ' || cRow.DoPledge);
 			DBMS_OUTPUT.PUT_LINE('Amount Received : ' || cRow.DoPaid);
-			DBMS_OUTPUT.PUT_LINE('*************************************');
-            DBMS_OUTPUT.NEW_LINE;
-            DBMS_OUTPUT.NEW_LINE;
       
 		END LOOP;
     
 	END EVENT_REPORT;
 
+
   PROCEDURE CLASS_REP_CONTACT_LIST IS
 
 		CURSOR crcReport IS
-    
+
 		SELECT d.CoordinatorID AS ClRep, d.LastName AS LName, 
 			d.FirstName AS FName, d.Phone AS PNum, a.City AS Cty, 
 			a.State AS Ste, a.Zip AS Zp, a.StreetAddress AS SAddress, 
 			a.AptNumber AS AptNum,
-      
       -- group by coodinatorID
-      -- last year donation info, this year's donation info
       TO_CHAR((SELECT SUM(Amount_Pledged) FROM Donation 
                LEFT JOIN Donor ON Donation.DonorID = Donor.DonorID
 						   LEFT JOIN EventDonors ON Donor.DonorID = EventDonors.DonorID
@@ -88,61 +134,13 @@ CREATE OR REPLACE PACKAGE BODY REPORTS IS
 			DBMS_OUTPUT.PUT_LINE('Apartment Number : ' || cRow.AptNum);
 			DBMS_OUTPUT.PUT_LINE('Donation Last Year : '  || cRow.LYDonation);
 			DBMS_OUTPUT.PUT_LINE('Donation This Year : '  || cRow.TYDonation);
-			DBMS_OUTPUT.PUT_LINE('*************************************');
-			DBMS_OUTPUT.NEW_LINE;
-			DBMS_OUTPUT.NEW_LINE;
+      DBMS_OUTPUT.PUT_LINE('*************************************');
+      DBMS_OUTPUT.NEW_LINE;
+      DBMS_OUTPUT.NEW_LINE;
 
 		END LOOP;
 
 	END CLASS_REP_CONTACT_LIST;
-
-  PROCEDURE MONTHLY_REPORT IS
-  
-      CURSOR mReports IS
-  
-      SELECT d.LastName AS LName, d.FirstName AS FName, a.City AS Cty, 
-          a.State AS Ste, a.Zip AS Zp, a.StreetAddress AS SAddress, 
-          a.AptNumber AS AptNum, TO_CHAR(do.Amount_Pledged - do.Amount_Paid, 
-          '9999999.99') AS Amt_Due, TO_CHAR(pp.DueDate, 'DD-MON-YY') AS DDate, 
-          TO_CHAR(do.Amount_Pledged, '9999999.99') AS Amt_Pledged, 
-          TO_CHAR(do.Amount_Paid, '9999999.99') AS Amt_Paid, 
-          TO_CHAR(pp.Last_Payment_Date, 'DD-MON-YY') AS LPayDate
-      FROM Donor d 
-      INNER JOIN Address a ON d.AddressID = a.AddressID
-      INNER JOIN Donation do ON d.DonorID = do.DonorID
-      INNER JOIN PaymentPlan pp ON do.DonationID = pp.DonationID
-      ORDER BY Last_Payment_Date;
-      
-      cRow mReports%ROWTYPE;	-- Cursor Row variable
-    
-    BEGIN
-    
-      FOR cRow IN mReports LOOP
-    
-          DBMS_OUTPUT.PUT_LINE('Name : ' || cRow.LName || ', ' || cRow.FName);
-          DBMS_OUTPUT.PUT_LINE('City : ' || cRow.Cty);
-          DBMS_OUTPUT.PUT_LINE('State: ' || cRow.Ste);
-          DBMS_OUTPUT.PUT_LINE('ZIP: ' || cRow.Zp);
-          DBMS_OUTPUT.PUT_LINE('Street Address : ' || cRow.SAddress);
-          DBMS_OUTPUT.PUT_LINE('Apartment Number : ' || cRow.AptNum);
-          DBMS_OUTPUT.PUT_LINE('Amount Due : ' || cRow.Amt_Due);
-          DBMS_OUTPUT.PUT_LINE('Date Due : ' || cRow.DDate);
-          DBMS_OUTPUT.PUT_LINE('Amount Pledged : ' || cRow.Amt_Pledged);
-          DBMS_OUTPUT.PUT_LINE('Amount Received : ' || cRow.Amt_Paid);
-          DBMS_OUTPUT.PUT_LINE('Date of Previous Payment : ' || cRow.LPayDate);
-          FOR j in 1..cRow.LPayDate LOOP
-          DBMS_OUTPUT.PUT_LINE('*');
-          END LOOP;
-          DBMS_OUTPUT.PUT_LINE('');
-          DBMS_OUTPUT.NEW_LINE;
-    
-      END LOOP;
-    
-    END MONTHLY_REPORT;
-    
-    /*Phonothon Volunteer Contact List Each volunteer caller is given a list with information about the potential donors he
-  or she is expected to call, including the name, telephone number, address, category, year (if applicable), and last year’s
-  donation information.*/
   
     PROCEDURE PHONOTHON_CONTACT_LIST IS
   
@@ -219,19 +217,78 @@ CREATE OR REPLACE PACKAGE BODY REPORTS IS
       
       END LOOP;
     CLOSE cCurse;
-END EMPLOYER_MATCHES; 
+  END EMPLOYER_MATCHES; 
+
+ 
+   /* Annual Report - This report lists the donor name along with their 
+  corresponding Donor Circle. If the total donations for a donor meet or exceed
+  the giving level, they will become a memeber of the respective Donor Circle.
+  
+  This report also displays summaries, including the total amount raised from 
+  all sources, the total for each class, the percent participation in each 
+  class, the total for each category, the grand total for each Donor Circle, and
+  the class total for each donor circle.*/
+  
+  PROCEDURE ANNUAL_REPORT IS
+  
+    BEGIN
+      DONORS_CIRCLES_LIST;
+      SUMMARY_ANNUAL_TOTALS;
+    END;
+    
+/* DONORS_CIRCLES_LIST - This Procedure is used within Annual Report. It grabs
+   the donor's name along with their respective donor circle. */
+    
+  PROCEDURE DONORS_CIRCLES_LIST IS
+  
+    CURSOR cDonorList IS -- Creates cursor for navigating through the result set
+      
+      SELECT LastName, 
+             FirstName, 
+             Circle_Level, 
+             Total_Donations
+      FROM Donor;
+      
+    rDonor cDonorList%ROWTYPE;
+      
+    BEGIN
+      OPEN cDonorList;
+      LOOP
+        FETCH cDonorList INTO rDonor;
+        EXIT WHEN cDonorList%NOTFOUND;
+        
+        DBMS_OUTPUT.PUT_LINE(rDonor.LastName || ' | ' || 
+                             rDonor.FirstName || ' | ' ||
+                             rDonor.Circle_Level || ' | ' ||
+                             rDonor.Total_Donations);
+        
+      END LOOP;
+      
+      CLOSE cDonorList;
+      
+      NULL;
+  END DONORS_CIRCLES_LIST;
+  
+/* SUMMARY_ANNUAL_TOTALS - Is called by the Annual Report, displaying calculated
+   summaries for the Year*/
+   
+  PROCEDURE SUMMARY_ANNUAL_TOTALS IS
+  
+    BEGIN
+      NULL;
+  END SUMMARY_ANNUAL_TOTALS;
 
 END REPORTS;
 /
 
-/*Suzanne would like a report each month listing the pledge payments that were due that month but were
-not received. It should list the donor’s name and address, the amount due, the date due, the amount of the pledge, the
-amount received so far, and the date of the previous payment, if any.*/
 
+/* -- TESTING -- */
 
+EXEC REPORTS.ANNUAL_REPORT;
+EXEC REPORTS.MONTHLY_REPORT ('2017', 'APR'); -- YYYY, MON
 EXEC REPORTS.EVENT_REPORT;
 EXEC REPORTS.CLASS_REP_CONTACT_LIST;
-EXEC REPORTS.MONTHLY_REPORT;
-
 EXEC REPORTS.PHONOTHON_CONTACT_LIST;
 EXEC REPORTS.EMPLOYER_MATCHES; 
+EXEC REPORTS.DONORS_CIRCLES_LIST;
+EXEC REPORTS.SUMMARY_ANNUAL_TOTALS;
